15647e39f0765e6ba66440e8d5c64d7be873ca02
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-26 09:53:51 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -69,7 +69,7 @@ st.download_button(
       key='download-csv'
       )
 st.table(df2)
-hist = alt.Chart(df2).mark_bar().encode(x = 'Date',
+hist = alt.Chart(df2).mark_bar().encode(x = 'index',
                                              y = 'Volume',title= 'No of Materials')
 st.subheader('Machine Distribution')
 st.altair_chart(hist,use_container_width=True)
